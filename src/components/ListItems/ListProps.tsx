d07abb7e6205207e726b2a0a0a68f47d6b5b7583
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2023-10-10 11:57:02 -0300

    Update ListProps.tsx
    Update Content List

--- src/components/ListItems/ListProps.tsx
+++ src/components/ListItems/ListProps.tsx
@@ -4,22 +4,22 @@ export const ListData = [
   {
       icon: <Layers className="w-10 text-white"/>,
       title: "Layers in Figma",
-      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better lilife"
+      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better life"
     },
   {
       icon: <Code className="w-10 text-white"/>,
       title: "Learn to code!",
-      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better lilife"
+      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better life"
     },
   {
       icon: <Figma className="w-10 text-white"/>,
       title: "Do prototype in Figma",
-      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better lilife"
+      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better life"
     },
   {
       icon: <User2 className="w-10 text-white"/>,
       title: "How to be a user?",
-      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better lilife"
+      content: "Builing the future with programming and others thing that we can do all days in the world and study more to better life"
     },
   ];
   
